f41e461641dc6ea2e6fb98ddd1f1829e6484b8b2
Chuhan <419436363@qq.com>
2016-03-22 15:14:58 -0500

    Webpack added

---
+++ my-app/src/app/app.js
@@ -0,0 +1,11 @@
+import angular from 'angular';
+
+angular.module('app', [require("angular-ui-router")])
+.config(function(($stateProvider, $urlRouterProvider))
+
+{
+	$urlRouterProvider.otherwise("/state1");
+	$stateProvider
+	.state('state1',{url:"/state1",template:"Adasd"})
+	.state('state2',{url:"/state2",template:"Asdasd"})
+});
