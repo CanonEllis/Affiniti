1529ea83f10b3214d5af18651a26ea767cf1a07b
Chuhan <419436363@qq.com>
2016-03-23 23:13:32 -0500

    finish draft templates

--- my-app/src/app/app.js
+++ my-app/src/app/app.js
@@ -1,11 +1,48 @@
 import angular from 'angular';
-
-angular.module('app', [require("angular-ui-router")])
+var myapp=angular.module('app', [require("angular-ui-router")])
 .config(function($stateProvider, $urlRouterProvider)
 
 {
-	$urlRouterProvider.otherwise("/state1");
+	$urlRouterProvider.otherwise("/home");
 	$stateProvider
-	.state('state1',{url:"/state1",template:"Adasd"})
-	.state('state2',{url:"/state2",template:"Asdasd"})
+	.state(
+	  'login',
+	    {
+		  url:"/login",
+			templateUrl:"templates/login.html",
+		    controller:"LoginCtrl"
+		}
+	
+	)
+	.state(
+	  'instruction',
+	   {
+		 url:"/instruction",
+		 templateUrl:"templates/instruction.html",
+		 controller:"SignupCtrl"
+		}
+	)
+	.state(
+		'home',
+		{
+			url:"^/home",
+			templateUrl:"templates/home.html",
+			controller:"HomeCtrl"
+		}
+	)
 });
+myapp.controller('AppCtrl',function($scope,$state){
+	
+	$scope.Instructions=function()
+	{
+		$state.go("instruction");
+	}
+	$scope.Home=function()
+	{
+		$state.go("home");
+	}
+	$scope.Login=function()
+	{
+		$state.go("login");
+	}
+})
