b947972dab3458024d7de3f40429697701b6d942
Canon Ellis <canone@smu.edu>
2016-03-30 18:33:24 -0500

    MxGraph files needed for creating the diagramming page

---
+++ my-app/src/app/controller.js
@@ -0,0 +1,10 @@
+import angular from 'angular';
+angular.module('Controllers',[])
+.controller('LoginCtrl',function($state,$scope,$http){
+	
+	
+})
+.controller('HomeCtrl',function($state,$scope,$http){
+	
+	
+})
