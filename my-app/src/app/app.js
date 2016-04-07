fa46f9678d5a3e9d2779033a87eb8b4711d14bae
Chuhan <419436363@qq.com>
2016-04-06 20:05:22 -0500

    4/6

--- my-app/src/app/app.js
+++ my-app/src/app/app.js
@@ -17,17 +17,17 @@ var myapp=angular.module('app', [require("angular-ui-router")])
 	.state(
 	  'about',
 	   {
-		 url:"/about",
+		 url:"^/about",
 		 templateUrl:"templates/aboutUS.html",
 		 controller:"aboutCtrl"
 		}
 	)
 	.state(
-		'landing',
+		'stepone',
 		{
-			url:"^/landing",
-			templateUrl:"templates/landingpage.html",
-			controller:"LandingCtrl"
+			url:"/stepone",
+			templateUrl:"templates/stepOne.html",
+			controller:"steponeCtrl"
 		}
 	)
 	.state(
@@ -38,9 +38,79 @@ var myapp=angular.module('app', [require("angular-ui-router")])
 			controller:"LoginCtrl"
 		}
 	)
+	.state(
+		'newproject',
+		{
+			url:"/newproject",
+			templateUrl:"templates/NewProject.html",
+			controller:"newprojectCtrl"
+		}
+	)
+	.state(
+		'steptwo',
+		{
+			url:"/steptwo",
+			templateUrl:"templates/simple-frame.html",
+			controller:"steptwoCtrl"
+		}
+	)
 });
-myapp.factory('User',function(){
-	return {data:{}};
+myapp.factory('User',function($http,$state,Respon){
+	
+	return {
+		Login:function(name,pass)
+		{
+			
+			return $http({
+				method: 'POST',
+				url: 'http://affiniti.us/login',
+				data:  $.param({ 'username' : name, 'password' :pass}),
+				headers:{'Content-Type':'application/x-www-form-urlencoded'}
+			}).then(function success(response){
+		       if(response.data["success"]=="yes")
+		        {
+					Respon.data = response.data;
+					Respon.username = Respon.data["username"];
+					Respon.email = Respon.data["email"];
+					$state.go("userProfile");
+				}
+		       else{ alert(response.data["success"]);
+			         //alert("wrong password or username");
+			      }
+			}, function error(response){
+				
+				alert("Connection Fail");
+			});
+		},
+		SignUp:function(first,last,name,pass,email)
+		{
+			return $http({
+				method:'POST',
+				url: ' http://affiniti.us/login',
+				data: $.param({'username':name,'password':pass,'first':first,'last':last,'email':email,'reemail':email}),
+				headers:{'Content-Type':'application/x-www-form-urlencoded'}
+			}).then(function success(response){
+				if(response.data["success"]=="yes")
+				{
+					$state.go("login");
+				}
+				else{
+					alert("somethingwrong");
+				}
+			}, function error(response){
+				alert("Connection Fail")
+			});
+		}
+		};
+	
+})
+.factory('Respon',function(){
+	return {
+		data:{},
+		username:"",
+		password:"",
+		email:""
+	 };
 })
 .controller('AppCtrl',function($scope,$state){
 	
@@ -58,47 +128,65 @@ myapp.factory('User',function(){
 	}
 	$scope.login=function()
 	{
-		$state.go("login")
+		$state.go("login");
 	}
 })
 .controller('LoginCtrl',function($state,$scope,$http,User){
+	
 	/*$scope.name = "jsmith";
-    $scope.email= "someone@smu.edu"
+    $scope.email= "someone@smu.edu"*/
 	$scope.log = function()
 	{
 		
-			$http({
-				method: 'GET',
-				url: 'http://private-d095eb-affiniti.apiary-mock.com/users/jsmith',
-				params:{
-				}
-				}).then(function successCallback(response){
-						//	$scope.firstname = response.firstName;
-							// $scope.lastname = response.LastName;
-							
-							User.data = response.data;
-							
-							if(User.data["password"]==$scope.password)
-							 {$state.go("userProfile");}
-							else {alert(User.data["password"])}
-							
-							 // $scope.res="Good"
-								}, function errorCallback(response){
-							
+		User.Login($scope.username,$scope.password);
+			//$state.go("userProfile");
 								
-								});
-	}*/
-	})
-	.controller('userCtrl',function($state,$scope,$http,User){
+	}
+	$scope.register=function()
+	{
+		User.SignUp($scope.firstname,$scope.lastname,$scope.username,$scope.password,$scope.email);
+	}
+})
+	.controller('userCtrl',function($state,$scope,$http,User,Respon){
 		
-		$scope.name = "jsmith";
-		$scope.email="some@smu.edu"
-		if(User.data!= null)
+		$scope.name = Respon.username;
+		$scope.gonew = function()
 		{
-			$scope.name = User.data["username"];
-			$scope.email = User.data["email"];
+			$state.go("newproject");
 		}
 		
 		
+	})
+	.controller('newprojectCtrl',function($state,$scope,User){
 		
+		$scope.gostepone = function()
+		{
+			$state.go("stepone");
+		}
+	})
+	.controller('steponeCtrl',function($state,$scope){
+		$scope.bulletpoints = [];
+		$scope.bullet="";
+		$scope.add= function()
+		{
+			
+			if ($scope.bulletpoints.indexOf($scope.bullet)<0 && $scope.bullet!="")
+			{
+				$scope.bulletpoints.push($scope.bullet);
+				$scope.bullet = "";
+			}
+			else {
+		     $scope.bullet="";
+			}
+			
+		}
+		$scope.delete = function(b)
+		{
+		   $scope.bulletpoints.splice($scope.bulletpoints.indexOf(b),1);
+		}
+		
+		$scope.gosteptwo=function()
+		{
+			$state.go("steptwo");
+		}
 	});
